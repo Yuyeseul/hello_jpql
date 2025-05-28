e0e8a43dbbdeddc9ab21d547b09b4514a2e9c97a
Yuyeseul <ryuys0308@naver.com>
2025-05-28 19:52:52 +0900

    프로젝션(SELECT)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -21,30 +21,33 @@ public class JpaMain {
             member.setAge(20);
             em.persist(member);
 
-            // 반환 타입 명확
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            // 반환 타입 명확 X
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
+            em.flush();
+            em.clear();
 
-            // 결과가 하나 이상일 때
-            List<Member> resultList = query1.getResultList();
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
+            // 영속성 컨텍스트 관리
+//            List<Member> result = em.createQuery("select m from Member m", Member.class)
+//                    .getResultList();
+//            Member findMember = result.get(0);
+//            findMember.setAge(25);
 
-            TypedQuery<Member> query4 = em.createQuery("select m from Member m where m.id = 20", Member.class);
+            // 프로젝션 여러 값 조회 1. Query 타입으로 조회
+//            List resultList = em.createQuery("select m.username, m.age from Member m").getResultList();
+//            Object o = resultList.get(0);
+//            Object[] result2 = (Object[]) o;
+//            System.out.println("username = " + result2[0]);
+//            System.out.println("age = " + result2[1]);
 
-            // 결과가 하나일 때
-            String singleResult = query2.getSingleResult();
-            System.out.println("singleResult = " + singleResult);
+            // 프로젝션 여러 값 조회 2. Object[] 타입으로 조회
+//            List<Object[]> resultList = em.createQuery("select m.username, m.age from Member m").getResultList();
+//            Object[] result2 = resultList.get(0);
+//            System.out.println("username = " + result2[0]);
+//            System.out.println("age = " + result2[1]);
 
-            // 파라미터 바인딩
-            TypedQuery<Member> query5 = em.createQuery("select m from Member m where m.username = :username", Member.class);
-            query5.setParameter("username", "member1");
-
-            Member singleResult1 = query5.getSingleResult();
-            System.out.println("singleResult1 = " + singleResult1.getUsername());
+            // 프로젝션 여러 값 조회 3. new 명령어로 조회
+            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList();
+            MemberDTO memberDTO = resultList.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
 
             tx.commit();
         } catch (
