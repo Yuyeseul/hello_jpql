fa991f7f5032d1bc02538bb3fe7886fb376317c7
Yuyeseul <ryuys0308@naver.com>
2025-06-02 17:24:27 +0900

    페치 조인 2 - 한계

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -41,30 +41,22 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            // 상태 필드
-            String query = "select m from Member m join fetch m.team"; // team 프록시 X
-            String query2 = "select distinct t from Team t join fetch t.members";
-            List<Member> resultList = em.createQuery(query, Member.class)
-                    .getResultList();
-            for (Member member : resultList) {
-                System.out.println("result : " + member.getUsername() + ", " + member.getTeam().getName());
-            }
+            String query1 = "select distinct t from Team t join fetch t.members"; // 페이징 API 사용 X
+            String query2 = "select t from Team t ";
 
-            List<Team> resultList2 = em.createQuery(query2, Team.class)
+            List<Team> resultList = em.createQuery(query2, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
                     .getResultList();
-            for (Team team : resultList2) {
-                System.out.println("result : " + team.getName() + ", " + team.getMembers().size());
+
+            System.out.println("resultList = " + resultList.size());
+
+            for (Team team : resultList) {
+                System.out.println("team : " + team.getName() + " | members :  " + team.getMembers().size());
                 for (Member member : team.getMembers()) {
                     System.out.println(" -> member = " + member);
                 }
             }
-
-            // "select m from Member m"
-            // member1 , teamA (SQL)
-            // member2 , teamA (1차 캐시)
-            // member3 , teamB (SQL)
-
-            // 회원 100명 -> 쿼리 100번 N(100번)+1(첫쿼리)
 
             tx.commit();
         } catch (
